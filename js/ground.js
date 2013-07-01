8633c4c1071a9f5d05b266e1e777fba12e6fa701
Ed Kelley <ekelleyv@gmail.com>
2013-07-01 15:14:19 -0400

    reorganization

---
+++ js/ground.js
@@ -0,0 +1,181 @@
+var Ground = function(offset, width, depth, start_height, end_height) {
+	this.start_height = start_height;
+	this.end_height = end_height;
+
+	this.width = width;
+	this.depth = depth;
+
+	this.width_segments = this.width/6;
+	this.height_segments = this.depth/6;
+
+	this.offset = offset;
+
+	//Parameters
+	this.max_prominence = 100;
+	this.min_height = 40;
+	this.num_spots = 10;
+	this.min_spot_width = 10;
+	this.max_spot_width = 30;
+	// this.inter_spot_noise = 3;
+	this.inter_spot_noise = 0;
+
+
+	this.material = this.init_material();
+	this.geometry = this.init_geometry();
+	this.mesh = this.init_mesh();
+}
+
+Ground.prototype.init_material = function() {
+	var material = Physijs.createMaterial(
+		new THREE.MeshLambertMaterial({ map: THREE.ImageUtils.loadTexture( 'images/dirt.png' ) }),
+		.8, // high friction
+		.3 // low restitution
+	);
+
+	material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+	material.map.repeat.set( 6 , 6 );
+
+	return material;
+};
+
+Ground.prototype.init_geometry = function() {
+	var geometry = new THREE.PlaneGeometry( this.width, this.depth, this.width_segments, this.height_segments );
+
+	var width_size = geometry.widthSegments+1;
+	var height_size = geometry.heightSegments+1;
+
+	//Generate the landing spot locations
+	var spots = [];
+	var range_size = this.width/this.num_spots - this.max_spot_width;
+	for (var i = 0; i < this.num_spots; i++) {
+		var spot = {};
+		var spot_width = Math.max(Math.random()*this.max_spot_width, this.min_spot_width);
+
+
+		//Endpoints for current window for generating landing spots
+		var low_range = i/this.num_spots*this.width - this.width/2;
+		var upper_range = low_range + range_size;
+
+		spot.x = range_size*Math.random() + low_range;
+		spot.y = Math.random()*this.max_prominence + this.min_height;
+		spot.width = spot_width;
+		spots.push(spot);
+	}
+
+	//Generate the terrain
+	var terrain_height = this.start_height;
+	var end_terrain_height = this.end_height;
+	var spot_count = 0;
+	var index = 0;
+	for ( var i = 0; i < height_size; i++ ) {
+		//TODO: FIX THE DIRECTION OF Y
+		//Because the mesh has been rotated, y is x
+		var x_pos = -1*geometry.vertices[index].y;
+		var spot = spots[spot_count];
+
+		//past_start : past the starting point of a landing spot
+		//past_end : past the end point of a landing spot
+		//If within a spot, terrain is flat
+		//If outside of a spot, move along the slope between the spots (with noise)
+		var past_start = (x_pos > spot.x);
+		var past_end = (x_pos > (spot.x + spot.width));
+
+
+		//If moving to next landing spot
+		if (!past_start) {
+			//The target height is the height of the next spot
+			var prev_height;
+			var target_height = spot.y;
+
+			var prev_x;
+			var target_x = spot.x;
+
+			//If between the start and the first landing spot
+			if (spot_count == 0) {
+				// prev_x = -this.width/2;
+				// prev_height = terrain_height;
+				terrain_height = this.start_height;
+			}
+			//If between any other two spots
+			else {
+				prev_x = spots[spot_count-1].x + spots[spot_count-1].width;
+				prev_height = spots[spot_count-1].y;
+
+				//Calculate the terrain height based on position between spots
+				terrain_height = (x_pos-prev_x)/(target_x-prev_x)*(target_height-prev_height) + prev_height;
+				
+				//Add noise to the inter-spot slope
+				terrain_height += (Math.random()-.5)*this.inter_spot_noise;
+			}
+
+
+		}
+		//If within the spot, make the terrain flat
+		else if (past_start && !past_end) {
+			terrain_height = spot.y;
+		}
+		//Move to the next spot
+		else {
+			//Next spot
+			if (spot_count < spots.length-1) {
+				past_start = false;
+				past_end = false;
+				spot_count++;
+			}
+			//Between last spot and end
+			else {
+				terrain_height = this.end_height;
+				// var prev_x = spots[spots.length-1].x + spots[spots.length-1].width;
+				// var prev_height = spots[spots.length-1].y;
+				// terrain_height = (x_pos-prev_x)/(this.end_height-prev_x)*(this.end_height-prev_height) + prev_height;
+			}
+		}
+
+		//Generate height based on the depth position
+		//TODO: Get rid of the magic numbers
+		for (var j = 0; j < width_size; j++) {
+			index = j + i*width_size;
+			var depth = geometry.vertices[index].x;
+
+			//Logistic function
+			var exp = -.1*(depth+80);
+			var height = terrain_height/(1+Math.pow(Math.E, exp));
+
+			//If after the landing depth, set to zero
+			if (depth > 20 && depth < 150) {
+				height = 0;
+			}
+			//For the background "mountain."
+			//TODO: Replace with something more interesting
+			else if (depth >= 150) {
+				height = depth - 150 + (Math.random()-.5)*10;
+			}
+
+			var noise = (past_start && !past_end) ? 0 : (Math.random()-.5)*this.inter_spot_noise;
+			geometry.vertices[index].z = height + noise;
+		}
+	}
+
+	geometry.computeFaceNormals();
+	geometry.computeVertexNormals();
+
+	return geometry;
+};
+
+Ground.prototype.init_mesh = function() {
+	var mesh = new Physijs.HeightfieldMesh(
+		this.geometry,
+		this.material,
+		0, // mass
+		this.geometry.widthSegments,
+		this.geometry.heightSegments
+	);
+
+	mesh.rotation.set(-Math.PI/2, 0, Math.PI/2);
+	mesh.position.set(this.offset, 0, 0);
+	mesh.receiveShadow = true;
+	mesh.castShadow = true;
+
+	return mesh;
+}
+
---
+++ js/lander.js
@@ -0,0 +1,246 @@
+var Lander = function() {
+	// this.material = this.initMaterial();
+	// this.geometry = this.initGeometry();
+	this.mesh = this.initMesh();
+	this.sparks = this.initSparks();
+	this.thrust_light = new THREE.SpotLight(0xFF7C00);
+	this.thrust_light.distance = 200;
+	this.thrust_on = false;
+}
+
+Lander.prototype.initSparks = function() {
+	var circle_size = 30;
+	var sparks = new THREE.Particles({
+                            size: circle_size,
+                            count: 300,
+                            position: new THREE.Vector3(0,0,0),
+                            color: "orange",
+                            program: function(context){
+	                           	var width = circle_size;
+	                           	var height = circle_size;
+	                           	var size = circle_size;
+								var gradient	= context.createRadialGradient( width/2, height /2, 0, width /2, height /2, width /2 );				
+								gradient.addColorStop( 0  , 'rgba(255,255,255,1)' );
+								gradient.addColorStop( 0.2, 'rgba(255,255,255,.4)' );
+								gradient.addColorStop( 0.4, 'rgba(128,128,128,.1)' );
+								gradient.addColorStop( 1  , 'rgba(0,0,0,0)' );
+
+								// gradient.addColorStop( 0  , 'rgba(0, 0, 255,1)' );
+								// gradient.addColorStop( 0.2, 'rgba(0, 0, 255,.4)' );
+								// gradient.addColorStop( 0.4, 'rgba(0, 0, 128,.1)' );
+								// gradient.addColorStop( 1  , 'rgba(0, 0, 0,0)' );
+
+								context.beginPath();
+								context.arc(size/2, size/2, size/2, 0, Math.PI*2, false);
+								context.closePath();
+
+								context.fillStyle = gradient;
+								context.fill();
+                            },
+                            sparksInit: function(emitter, SPARKS){
+
+                                emitter.addInitializer(new SPARKS.Position( new SPARKS.PointZone( new THREE.Vector3(0,0,0) ) ) );								emitter.addInitializer(new SPARKS.Lifetime(0,0.1));
+								emitter.addInitializer(new SPARKS.Velocity(new SPARKS.PointZone(new THREE.Vector3(0, -100, 0))));
+
+								emitter.addAction(new SPARKS.Age());
+								emitter.addAction(new SPARKS.Move());
+								emitter.addAction(new SPARKS.RandomDrift(2000,0,2000));
+								emitter.addAction(new SPARKS.Accelerate(0,-200,0));
+                            }
+                          });
+	return sparks;
+};
+
+Lander.prototype.initMaterial = function() {
+	var material = Physijs.createMaterial(
+		new THREE.MeshLambertMaterial(),
+		.8, // high friction
+		.4 // low restitution
+	);
+	material.color.setRGB( .8, .8, .8);
+	return material;
+};
+
+Lander.prototype.initGeometry = function() {
+	var geometry = new THREE.CubeGeometry(4.3, 4.1, 5.5);
+	return geometry;
+};
+
+Lander.prototype.initMesh = function() {
+	// var mesh = new Physijs.BoxMesh(
+	// 	this.geometry,
+	// 	this.material,
+	// 	50 // mass
+	// );
+
+	// mesh.position.set(5, 150, 0);
+	// mesh.receiveShadow = true;
+
+	// return mesh;
+	var top_material = Physijs.createMaterial(
+		new THREE.MeshLambertMaterial({ color: new THREE.Color(0xFFFFFF)}),
+		.8, // high friction
+		.4 // low restitution
+	);
+
+	top_material.shading = THREE.FlatShading;
+	// material.map.wrapS = material.map.wrapT = THREE.RepeatWrapping;
+	// material.map.repeat.set( .05, .05 );
+
+	var bottom_material = Physijs.createMaterial(
+		new THREE.MeshLambertMaterial({ color: new THREE.Color(0xCF8100)}),
+		.8, // high friction
+		.4 // low restitution
+	);
+	bottom_material.shading = THREE.FlatShading;
+
+	var thruster_material = Physijs.createMaterial(
+		new THREE.MeshLambertMaterial({ color: new THREE.Color(0x111111)}),
+		.8, // high friction
+		.4 // low restitution
+	);
+
+	var lander = new Physijs.CylinderMesh(
+		// CylinderGeometry(radiusTop, radiusBottom, height, radiusSegments, heightSegments, openEnded)
+		new THREE.CylinderGeometry(5, 5, 3, 8, 10, false),
+		bottom_material,
+		100
+		);
+	lander.castShadow = true;
+	lander.receiveShadow = true;
+
+	lander.position.z = 30;
+	lander.position.y = 200;
+
+	
+
+	var _object;
+	var top = new Physijs.CylinderMesh(
+		
+		new THREE.IcosahedronGeometry(5, .3),
+		top_material,
+		100
+		);
+
+	top.position.y = 3;
+	top.castShadow = true;
+	top.receiveShadow = true;
+	lander.add(top);
+
+
+	var r_leg = new Physijs.BoxMesh(
+		
+		new THREE.CubeGeometry(.5, 10, .5, 1, 1, 1),
+		top_material,
+		10
+	);
+
+	r_leg.position.x = 3.5;
+	r_leg.position.y = 1;
+	r_leg.rotation.z = .3;
+	r_leg.castShadow = true;
+	r_leg.receiveShadow = true;
+	lander.add(r_leg);
+
+	var l_leg = new Physijs.BoxMesh(
+		
+		new THREE.CubeGeometry(.5, 10, .5, 1, 1, 1),
+		top_material,
+		10
+	);
+
+	l_leg.position.x = -3.5;
+	l_leg.position.y = 1;
+	l_leg.rotation.z = -.3;
+	l_leg.castShadow = true;
+	l_leg.receiveShadow = true;
+	lander.add(l_leg);
+
+	var f_leg = new Physijs.BoxMesh(
+		
+		new THREE.CubeGeometry(.5, 10, .5, 1, 1, 1),
+		top_material,
+		10
+	);
+
+	f_leg.position.z = 3.5;
+	f_leg.position.y = 1;
+	f_leg.rotation.x = -.3;
+	f_leg.castShadow = true;
+	f_leg.receiveShadow = true;
+	lander.add(f_leg);
+
+	var f_leg = new Physijs.BoxMesh(
+		
+		new THREE.CubeGeometry(.5, 10, .5, 1, 1, 1),
+		top_material,
+		10
+	);
+
+	f_leg.position.z = -3.5;
+	f_leg.position.y = 1;
+	f_leg.rotation.x = .3;
+	f_leg.castShadow = true;
+	f_leg.receiveShadow = true;
+	lander.add(f_leg);
+
+	var thruster = new Physijs.CylinderMesh(
+		new THREE.CylinderGeometry(.5, 2, 3, 30, 10, false),
+		thruster_material,
+		10
+	);
+
+	thruster.position.y = -1;
+	thruster.castShadow = true;
+	thruster.receiveShadow = true;
+	lander.add(thruster);
+
+
+
+	return lander;
+}
+
+Lander.prototype.apply_thrust = function() {
+	var strength = 2000;
+	var rotation_matrix = new THREE.Matrix4();
+	rotation_matrix.extractRotation(this.mesh.matrix);
+
+	var force_vector = new THREE.Vector4(0, strength, 0, 1);
+	force_vector.applyMatrix4(rotation_matrix);
+	this.mesh.applyCentralForce(force_vector);
+}
+
+Lander.prototype.rotate_left = function() {
+	this.apply_rotation(.1);
+}
+
+Lander.prototype.rotate_right = function() {
+	this.apply_rotation(-.1);
+}
+
+Lander.prototype.apply_rotation = function(amount) {
+	var old_vector = this.mesh.getAngularVelocity();
+	var rot_vector = new THREE.Vector3(0, 0, amount);
+	var new_vector = new THREE.Vector3();
+	new_vector.addVectors(old_vector, rot_vector);
+	this.mesh.setAngularVelocity(new_vector);
+}
+
+Lander.prototype.update_sparks = function() {
+	this.sparks.position = this.mesh.position;
+	this.sparks.rotation.z = this.mesh.rotation.z;
+
+	this.thrust_light.position = this.sparks.position;
+
+	var rotation_matrix = new THREE.Matrix4();
+	rotation_matrix.extractRotation(this.mesh.matrix);
+	var offset = new THREE.Vector4(0, -10, 0);
+	offset.applyMatrix4(rotation_matrix);
+
+	this.thrust_light.target.position = offset;
+	this.thrust_light.target.position.addVectors(this.mesh.position, offset);
+	// console.log(this.thrust_light.target.position);
+
+	this.sparks.visible = this.thrust_on;
+	this.thrust_light.visible = this.thrust_on;
+}
